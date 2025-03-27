70747ed0a0c035f6225832509614c48aedbcaeb5
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-03-27 21:38:31 +0700

    first commit

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int arr[20]; // membuat array dengan panjang data 20
+int n; // membuat variable inputan n
+
+void input()
+{ // procedure input 
+while (true)
+{
+cout << "masukan jumlah data pada array : "; // membuat inputan jumlah element array
+cin >> n; // memanggil variable inputan n
+
+if (n <= 20)
+{ // membuat kondisi n tidak lebih dari 20 
+
+}
