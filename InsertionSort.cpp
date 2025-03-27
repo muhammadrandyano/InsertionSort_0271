527bf49ed21b9ee283eb75fb927382561c46b289
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-03-27 21:47:53 +0700

    first commit

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -13,5 +13,18 @@ cin >> n; // memanggil variable inputan n
 
 if (n <= 20)
 { // membuat kondisi n tidak lebih dari 20 
-
+break;
 }
+else
+{
+    cout << "\nArray yang anda masukan maksimal 20 element.\n"; // menampilkan pesan jika data lebih dari 20
+}
+}
+cout << endl; //membuat jarak per baris program 
+cout << "=====================" << endl; // membuat tampilkan susunan data element array
+cout << "masukan element array" << endl;
+cout << "=====================" << endl;
+
+for (int i = 0; i < n; i++) // menggunakan perulangan for untuk menyimpan data pada array 
+
+cout << "Data ke-" << (i+1) << ": "; // memasukan atau menginputkan nilai data n
