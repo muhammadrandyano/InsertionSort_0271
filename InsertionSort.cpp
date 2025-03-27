79415c4ed98ae15be881684f05baeaf15e76bbb7
muhammadrandyano <muhammadrandyyano@gmail.com>
2025-03-27 22:04:37 +0700

    first commit

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -42,3 +42,19 @@ for (i = 1; i <= n - 1; i++)
 { // step 1 
 
 temp = arr[i]; // step 2
+
+j = i - 1; // step 3
+
+while (j >= 0 && arr[j] > temp) // step 4 
+{
+    (arr[j + 1] = arr[j] > temp)
+    j--; // step 4b
+}
+
+arr [j + 1] = temp; // step 5
+}
+}
+
+void display()
+{
+    cout << endl; // output baris kosong
